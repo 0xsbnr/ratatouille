cbd8b799e83914f6faea638fc15884cbfa0b5a44
LeatherPotato <thesussywussy06@gmail.com>
2026-03-14 18:26:31 +0000

    stuff

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,33 @@
 import logo from './logo.svg';
 import './App.css';
+import Cookies from "js-cookie";
 
-function App() {
-  return (
-    <div className="App">
-      Hello World
-    </div>
-  );
+import LoginPage from './pages/Login';
+
+
+//function App() {
+//  return (
+//    <div className="App">
+//      Hello World
+//    </div>
+//  );
+//}
+
+const App = () => {
+    let sessionID = Cookies.get('session_id')
+    if (sessionID===-1 || sessionID===undefined) {
+        return (
+            <div>
+                user isnt logged in
+            </div>
+        )
+    } else {
+        return (
+            <div>
+                send user to /home or smth
+            </div>
+        )
+    }
 }
 
 export default App;
--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,7 +1,16 @@
-function LoginPage() {
+import React from "react";
+import "../App.css"
+import GLOBALS from "../Global"
+import Cookies from "js-cookie";
+
+class LoginPage extends React.Component {
+  render() {
   return (
     <div className="LandingPage">
       Hello World
     </div>
   );
+  }
 }
+
+export default LoginPage;
