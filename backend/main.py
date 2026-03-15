45fe30ffeb94a72603870c66099af3742b834ea0
Ismahaan17 <168064421+Ismahaan17@users.noreply.github.com>
2026-03-15 05:34:28 +0000

    done with python thing

--- backend/main.py
+++ backend/main.py
@@ -1,16 +1,15 @@
 from flask import Flask
-#from db import Database
+from db import Database
 import json
 from flask_cors import CORS
 import our_types
 
-#database = Database("i")
+database = Database("i")
 
 app = Flask(__name__)
 CORS(app, origins=["https://localhost:8080", "http://localhost:8080",
      "https://localhost:3000", "http://localhost:3000", "192.168.0.37"])
 
-bad_data = { "yo" : "no" }
 
 @app.route("/")
 def hello_world():
@@ -19,53 +18,53 @@ def hello_world():
 
 
 @app.route('/api/login', methods=['GET', 'POST'])
-def login(username, password):
+def login():
     if request.is_json:
         data = request.json
-        return json.dumps(bad_data)
+        return json.dumps(database.log_in(username=data["username"],password=data["password"]))
     else:
         return json.dumps("ERR Content type is not supported."), 418
 
 
 @app.route('/api/signup', methods=['GET', 'POST'])
-def signup(username, password):
+def signup():
     if request.is_json:
         data = request.json
-        return json.dumps(bad_data)
+        return json.dumps(database.sign_up(username=data["username"],password=data["password"])
     else:
         return json.dumps("ERR Content type is not supported."), 418
 
 
 @app.route('/api/get_groups', methods=['GET', 'POST'])
-def get_groups(username, password):
+def get_groups():
     if request.is_json:
         data = request.json
-        return json.dumps(bad_data)
+        return json.dumps(database.get_groups(user_id=data["user_id"]))
     else:
         return json.dumps("ERR Content type is not supported."), 418
     
 
 
 @app.route('/api/join_group', methods=['GET', 'POST'])
-def join_group(username, password):
+def join_group():
     if request.is_json:
         data = request.json
-        return json.dumps(bad_data)
+        return json.dumps(database.join_group(user_id=data["user_id"],group_id=data["group_id"]))
     else:
         return json.dumps("ERR Content type is not supported."), 418
 
 
 @app.route('/api/send_drawing', methods=['GET', 'POST'])
-def send_drawing(username, password):
+def send_drawing():
     if request.is_json:
         data = request.json
-        return json.dumps(bad_data)
+        return json.dumps(database.add_square(user_id=["user_id"],file_path=data["file_path"],complete=data["complete"]))
     else:
 
         return json.dumps("ERR Content type is not supported."), 418
 
 @app.route('/api/get_full_drawing', methods=['GET', 'POST'])
-def get_full_drawing(username, password):
+def get_full_drawing():
     if request.is_json:
         data = request.json
         return json.dumps(bad_data)
