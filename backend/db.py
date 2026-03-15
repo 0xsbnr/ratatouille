850ec3eadb329ebba9026e518648bb76839a96b4
Ismahaan17 <168064421+Ismahaan17@users.noreply.github.com>
2026-03-15 04:11:54 +0000

    i wrote code, im proud of myself. I wrote code to do with a whiteboard

--- backend/db.py
+++ backend/db.py
@@ -1,10 +1,4 @@
-class Database:
-    import sqlite3
-    from our_types import ...
-    import datetime
 
-    def __init__(self, db_path):
-        self.con = self.sqlite3
 
 
 
--- backend/main.py
+++ backend/main.py
@@ -1,24 +1,91 @@
 from flask import Flask
-from db import Database
+#from db import Database
 import json
 from flask_cors import CORS
 import our_types
 
-database = Database("i")
+#database = Database("i")
 
 app = Flask(__name__)
 CORS(app, origins=["https://localhost:8080", "http://localhost:8080",
      "https://localhost:3000", "http://localhost:3000", "192.168.0.37"])
+
+bad_data = { "yo" : "no" }
 
 @app.route("/")
 def hello_world():
     return "<p>Hello, World!</p>"
 
 
-@app.route('/api/get_thing/', methods=['GET', 'POST'])
-def get_thing():
+
+@app.route('/api/login', methods=['GET', 'POST'])
+def login(username, password):
     if request.is_json:
         data = request.json
-        return json.dumps(db.get_thing(argument=data.argument_name))
+        return json.dumps(bad_data)
     else:
         return json.dumps("ERR Content type is not supported."), 418
+
+
+@app.route('/api/signup', methods=['GET', 'POST'])
+def signup(username, password):
+    if request.is_json:
+        data = request.json
+        return json.dumps(bad_data)
+    else:
+        return json.dumps("ERR Content type is not supported."), 418
+
+
+@app.route('/api/get_groups', methods=['GET', 'POST'])
+def get_groups(username, password):
+    if request.is_json:
+        data = request.json
+        return json.dumps(bad_data)
+    else:
+        return json.dumps("ERR Content type is not supported."), 418
+    
+
+
+@app.route('/api/join_group', methods=['GET', 'POST'])
+def join_group(username, password):
+    if request.is_json:
+        data = request.json
+        return json.dumps(bad_data)
+    else:
+        return json.dumps("ERR Content type is not supported."), 418
+
+
+@app.route('/api/send_drawing', methods=['GET', 'POST'])
+def send_drawing(username, password):
+    if request.is_json:
+        data = request.json
+        return json.dumps(bad_data)
+    else:
+
+        return json.dumps("ERR Content type is not supported."), 418
+
+@app.route('/api/get_full_drawing', methods=['GET', 'POST'])
+def get_full_drawing(username, password):
+    if request.is_json:
+        data = request.json
+        return json.dumps(bad_data)
+    else:
+
+        return json.dumps("ERR Content type is not supported."), 418
+    
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
